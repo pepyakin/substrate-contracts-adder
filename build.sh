fa4e1e36861f230df26c500f7ecc1abd5150f7df
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-09-30 18:43:25 +0100

    Invoke wasm-build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cargo +nightly build --release --target=wasm32-unknown-unknown --verbose
+wasm-build target substrate-contracts-adder --target-runtime=substrate --final=adder --save-raw=./target/adder-deployed.wasm --target wasm32-unknown-unknown
